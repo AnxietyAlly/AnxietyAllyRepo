2320633ebe80e2f3fda8af4b88d84d0cccf55294
ItsDimitrie <laue0002@HZ.nl>
2023-10-31 13:47:38 +0100

    Frontend pages added.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,19 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  extensions: ['.svelte'],
+  preprocess: [vitePreprocess()],
+  kit: {
+    // Specify the base path if necessary, for example:
+    // basePath: '/your-subdirectory',
+
+    // Add the following `files` section to serve the "static" directory.
+    files: {
+      assets: 'static',
+    },
+
+    // Specify the adapter as adapter-auto.
+    adapter: adapter(),
+  },
 };
+
 export default config;
