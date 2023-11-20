1fcd2e17fc23f7dfed1997fe3fa86ba1857d64e7
juliapieters <piet0058@hz.nl>
2023-11-20 13:23:38 +0100

    add articles to home page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -35,25 +35,25 @@
 	  <h2 class="text-xl italic">Articles</h2>
 	  <div class="flex flex-wrap">
 		<div class="w-80 bg-indigo-100 my-2 grid grid-cols-3 gap-2 rounded-md">
-		  <img class="h-30" src=".//help-wanted-button.png" alt="Anxiety Ally Logo">
-		  <div class="col-span-2">
-			<h3 class="font-bold">How anxiety hurts you</h3>
-			<p>blabla</p>
-		  </div>
+		  <img class="h-full object-cover" src="https://geauxgirlmagazine.org/wp-new/wp-content/uploads/2023/05/gg15-my-social-anxiety.jpg" alt="Artwork about a woman crying">
+		  <a href="https://geauxgirlmagazine.org/my-experience-with-social-anxiety/" class="col-span-2">
+			<h3 class="font-bold">My Experience with Social Anxiety</h3>
+			<p>According to the Anxiety and Depression Association of America, social anxiety is indicated by a phobia of... Read more</p>
+		  </a>
 		</div>
 		<div class="w-80 bg-indigo-100 my-2 grid grid-cols-3 gap-2 rounded-md">
-		  <img class="h-30" src=".//help-wanted-button.png" alt="Anxiety Ally Logo">
-		  <div class="col-span-2">
-			<h3 class="font-bold">test</h3>
-			<p>blabla</p>
-		  </div>
+		  <img class="h-full object-cover" src="https://www.youngminds.org.uk/media/3p3hinx3/1832.png?anchor=center&mode=crop&width=1920&height=1080&rnd=Wide_1920_1080&format=webp&quality=67" alt="Young man having anxiety">
+		  <a href="https://www.youngminds.org.uk/young-person/blog/how-i-learnt-to-cope-with-sports-performance-anxiety/" class="col-span-2">
+			<h3 class="font-bold">How I learnt to cope with sports performance anxiety</h3>
+			<p>Paddy shares his experience of sports performance anxiety and how it almost stopped him from playing rugby... Read more</p>
+		  </a>
 		</div>
 		<div class="w-80 bg-indigo-100 my-2 grid grid-cols-3 gap-2 rounded-md">
-		  <img class="h-30" src=".//help-wanted-button.png" alt="Anxiety Ally Logo">
-		  <div class="col-span-2">
-			<h3 class="font-bold">test</h3>
-			<p>blabla</p>
-		  </div>
+		  <img class="h-full object-cover" src="https://thelightprogram.pyramidhealthcarepa.com/wp-content/uploads/2022/01/angry-girl-waiting-for-a-mobile-phone-call-stockpack-istock-2048x1365.jpg" alt="Angry girl waiting for phone call">
+		  <a href="https://thelightprogram.pyramidhealthcarepa.com/what-is-separation-anxiety-in-adults/" class="col-span-2">
+			<h3 class="font-bold">What is Separation Anxiety in Adults?</h3>
+			<p>When we talk about separation anxiety, most people assume you are talking about a condition children experience but... Read more</p>
+		  </a>
 		</div>
 	  </div>
 	</div>
