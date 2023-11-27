b281b4fc766b4664b1763c94a0a754a0b3410e86
amyvdhart <112553946+amyvdhart@users.noreply.github.com>
2023-11-27 09:33:08 +0100

    fixed layout

--- frontend/src/routes/questionnaire/+page.svelte
+++ frontend/src/routes/questionnaire/+page.svelte
@@ -141,29 +141,32 @@
 	}
 </script>
 <div class="min-h-screen bg-sky flex flex-col items-center">
-    <img class="w-40 h-100" src=".//anxietyAllyLogo.png" alt="Anxiety Ally Logo">
+    <img class="w-40 h-100 z-10" src=".//anxietyAllyLogo.png" alt="Anxiety Ally Logo">
 <!--This is the start screen, probably not going to use it-->
-<div class="app top-0 items-center mt-64 px-10 py-2 rounded-full focus:outline-none p-50 justify-between flex-wrap w-45 border-radius-20 my-10 text-white fixed bottom-0 left-0 w-full h-16 flex w-150 bg-blue-300 rounded-10 overflow-hidden flex-col">
+<div class="app top-0">
 	{#if questionPointer==-1}
-		<div class="start-screen flex justify-center items-center px-10 py-2 bg-blue-500 text-white rounded-full focus:outline-none">
 			
-			<button class="px-10 py-2 bg-blue-500 text-white rounded-full focus:outline-none w-45 border-radius-20 my-10" on:click={()=>{questionPointer=0}}>
-				Start Quiz
-			</button>	
-		</div>
+	<div class="bg-blue-400 text-black rounded-lg w-20">
+		Begin your journey to anxiety management and inner peace by answering questions in our questionnaire.
+		Identify your anxiety, vent to our AI doctor, and connect with local psychologists. Take the first steps toward a calmer, more serene you
+	</div>
+	
+<button class="bg-blue-500 text-white rounded-full focus:outline-none w-45 border-radius-20 ml-36 h-14 w-24 mt-36" on:click={()=>{questionPointer=0}}>
+	Start Quiz
+</button>
+		
 <!--These are the quiz screens with the questions-->
 	{:else if !(questionPointer > answers.length-1)}
-	
-		<div class="quiz-screen p-50 bg-sky flex-wrap w-45 border-radius-20 my-10 bg-blue-400 text-white fixed bottom-0 left-0 w-full flex justify-between items-center w-150 h-10 rounded-10 overflow-hidden">
-		
-			<div class="main p-4 flex justify-between flex-wrap mx-auto rounded-lg mt-12 bg-blue-400 text-white h-56 absolute">		
+		<div class="quiz-screen p-50 bg-sky flex-wrap w-45 border-radius-20 my-10 bg-blue-400 text-black fixed bottom-0 left-0 w-full flex justify-between items-center w-150 h-10 rounded-10 overflow-hidden">
+			
+			<div class="main p-4 flex justify-between flex-wrap mx-auto rounded-lg mt-12 bg-blue-400 text-black h-56 absolute">		
 					
 				<h2>
 					{questions[questionPointer].question}
 				</h2>
-				<div class="options flex justify-between flex-wrap w-45 border-radius-20 my-10 bg-blue-400 text-white">
+				<div class="options flex justify-between flex-wrap w-45 border-radius-20 my-10 bg-blue-400 text-black">
 					{#each questions[questionPointer].options as opt,i}
-						<button class="{answers[questionPointer]==i?'selected':''} px-10 py-2 bg-blue-500 text-white rounded-full focus:outline-none w-45 border-radius-20 my-10" on:click={()=>{answers[questionPointer]=i}}>
+						<button class="{answers[questionPointer]==i?'selected':''} px-10 py-2 bg-blue-500 text-black rounded-full focus:outline-none w-45 border-radius-20 my-10" on:click={()=>{answers[questionPointer]=i}}>
 							{opt}	
 						</button>
 					{/each}
@@ -187,16 +190,21 @@
 			</div>
 		</div>
 		<!-- this is the score page -->
-	{:else}
-		<div class="score-screen flex justify-center items-center px-10 py-2 bg-blue-500 text-white rounded-full focus:outline-none h-72">
-			<h2 class="text-white mt-4 z-10 p-0">
-				 {getScore()}
-			</h2>
-			 <button class=" bg-blue-500 text-white rounded-full focus:outline-none w-45 border-radius-20 mr-6 mt-7" on:click={restartQuiz}>
+		{:else}
+		<div class="score-screen bg-blue-400 text-black rounded-lg w-20 mt-10">
+			After completing this test, it seems you might have some symptoms related to:
+		</div>
+	
+		<div class="score-container flex flex-col items-center mt-4">
+			<div class="score bg-blue-500 text-white rounded-full flex items-center justify-center">
+				Social Anxiety with a score of: {getScore()}
+			</div>
+			<button class="bg-blue-500 text-white rounded-full focus:outline-none w-45 border-radius-20 ml-7 h-14 w-24 mt-36" on:click={restartQuiz}>
 				Restart Quiz
 			</button>
-</div>
+		</div>
 	{/if}
+	
 </div>
 </div>
 <style>
